ecf94db77ee51964f8390018bfa12586cf7773b7
Debora Aline Silva <debora.aline@agilize.com.br>
2022-09-21 16:55:14 -0300

    Finaliza model aluno

--- app/Packages/Aluno/Models/Aluno.php
+++ app/Packages/Aluno/Models/Aluno.php
@@ -3,6 +3,7 @@
 namespace App\Packages\Aluno\Models;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Illuminate\Support\Str;
 
 /**
@@ -11,18 +12,62 @@ use Illuminate\Support\Str;
  */
 class Aluno
 {
-
-    public function __construct()
-    {
-        $this->id = Str::uuid()->toString();
-    }
-
+    use TimestampableEntity;
     /**
      * @ORM\Id
      * @ORM\Column(type="uuid", unique=true)
      */
     protected string $id;
 
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    protected string $nome;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    protected string $email;
+
+    /**
+     * @var integer
+     * @ORM\Column(type="integer")
+     */
+    protected int $telefone;
+
+    public function __construct(string $nome, string $email, int $telefone)
+    {
+        $this->id = Str::uuid()->toString();
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->telefone = $telefone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTelefone(): int
+    {
+        return $this->telefone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
 
     /**
      * @return string
