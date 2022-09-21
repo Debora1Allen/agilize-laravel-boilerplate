5d2767303bb658ee3c3beed8fa40a982b28b464e
Debora Aline Silva <debora.aline@agilize.com.br>
2022-09-21 18:51:42 -0300

    Estrutura models

--- app/Packages/Prova/Models/Questao.php
+++ app/Packages/Prova/Models/Questao.php
@@ -2,7 +2,23 @@
 
 namespace App\Packages\Prova\Models;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="questoes")
+ */
 class Questao
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     */
+    protected string $id;
 
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    protected string $texto;
 }
--- app/Packages/Prova/Models/Tema.php
+++ app/Packages/Prova/Models/Tema.php
@@ -2,7 +2,23 @@
 
 namespace App\Packages\Prova\Models;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tema")
+ */
 class Tema
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     */
+    protected string $id;
 
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    protected string $nome;
 }
