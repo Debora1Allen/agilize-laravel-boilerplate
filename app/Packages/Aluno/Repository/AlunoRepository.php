78a677d95ae80dca344e680f916bbeb0569e310e
Debora Aline Silva <debora.aline@agilize.com.br>
2022-09-22 18:45:43 -0300

    Cria Repositorys de Prova e Aluno

--- app/Packages/Aluno/Repository/AlunoRepository.php
+++ app/Packages/Aluno/Repository/AlunoRepository.php
@@ -2,7 +2,25 @@
 
 namespace App\Packages\Aluno\Repository;
 
-class AlunoRepository
-{
+use App\Packages\Aluno\Models\Aluno;
+use App\Packages\Base\Repository\AbstractRepository;
 
+class AlunoRepository extends AbstractRepository
+{
+    public function findAllAlunos()
+    {
+        return $this->findAll();
+    }
+
+    public function createAluno(Aluno $aluno)
+    {
+        $this->getEntityManager()->persist($aluno);
+        $this->getEntityManager()->flush();
+        return $aluno;
+    }
+
+    public function findOneAlunoById(string $id): ?Aluno
+    {
+        return $this->findOneBy(['id' => $id]);
+    }
 }
--- app/Packages/Prova/Repository/ProvaRepository.php
+++ app/Packages/Prova/Repository/ProvaRepository.php
@@ -2,7 +2,22 @@
 
 namespace App\Packages\Prova\Repository;
 
-class ProvaRepository
+use App\Packages\Aluno\Models\Aluno;
+use App\Packages\Base\Repository\AbstractRepository;
+use App\Packages\Prova\Models\Prova;
+
+class ProvaRepository extends AbstractRepository
 {
 
+    public function createProva(Prova $prova)
+    {
+        $this->getEntityManager()->persist($prova);
+        $this->getEntityManager()->flush();
+        return $prova;
+    }
+
+    public function findOneProvaById(string $id): ?Prova
+    {
+        return $this->findOneBy(['id' => $id]);
+    }
 }
