1cdacd0f39b632fa3b07b67a473045dd745ce312
Debora Aline Silva <debora.aline@agilize.com.br>
2022-09-27 16:30:15 -0300

    cria Seed Aluno

--- database/seeders/Aluno.php
+++ database/seeders/Aluno.php
@@ -4,9 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use LaravelDoctrine\ORM\Facades\EntityManager;
 
 class Aluno extends Seeder
 {
+    /**
+     * @var \App\Packages\Aluno\Models\Aluno
+     */
+    protected \App\Packages\Aluno\Models\Aluno $aluno;
     /**
      * Run the database seeds.
      *
@@ -14,6 +19,12 @@ class Aluno extends Seeder
      */
     public function run()
     {
-        //
+
+        $nome = $this->aluno->getNome() ;
+        $telenone = $this->aluno->getTelefone();
+        $email = $this->aluno->getEmail() ;
+        EntityManager::persist(new \App\Packages\Aluno\Models\Aluno($nome,$email,$telenone));
+
+        EntityManager::flush();
     }
 }
