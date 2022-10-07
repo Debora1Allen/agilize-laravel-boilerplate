9d35bf3bbc67dc9d96d7d786597cff35fb01c76a
Debora Aline Silva <debora.aline@agilize.com.br>
2022-10-07 08:58:36 -0300

    repository para snapshot

--- app/Packages/Prova/Repository/SnapshotRepository.php
+++ app/Packages/Prova/Repository/SnapshotRepository.php
@@ -3,12 +3,15 @@
 namespace App\Packages\Prova\Repository;
 
 use App\Packages\Base\Repository\AbstractRepository;
+use App\Packages\Prova\Models\Prova;
 
 class SnapshotRepository extends AbstractRepository
 {
 
-    public function add($prova)
+    public function add(Prova $prova):Prova
     {
-
+        $this->getEntityManager()->persist($prova);
+        $this->getEntityManager()->flush();
+        return $prova;
     }
 }
